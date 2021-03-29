36b4fcce3460bd9458720db1f63a5738cfed3489
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-03-29 12:51:45 +0200

    Updated config files for simpler preprocessing.

--- config/ubuntu_16lts.cmake
+++ config/ubuntu_16lts.cmake
@@ -9,9 +9,10 @@ else()
   set(ENV{FC}  gfortran) # Fortran compiler for serial build
 endif()
 
-set(USER_CXX_FLAGS "-std=c++14 -DBOOL_TYPE=\"signed char\"")
+set(USER_CXX_FLAGS "-std=c++14")
 set(USER_CXX_FLAGS_RELEASE "-O3 -DNDEBUG -march=native")
 set(USER_CXX_FLAGS_DEBUG "-O0 -g -Wall -Wno-unknown-pragmas")
+
 set(USER_FC_FLAGS "-std=f2003 -fdefault-real-8 -fdefault-double-8 -fPIC -ffixed-line-length-none -fno-range-check")
 set(USER_FC_FLAGS_RELEASE "-O3 -DNDEBUG -march=native")
 set(USER_FC_FLAGS_DEBUG "-O0 -g -Wall -Wno-unknown-pragmas")
@@ -24,5 +25,5 @@ set(HDF5_LIB_2         "/usr/lib/x86_64-linux-gnu/libhdf5_serial_hl.so")
 set(LIBS ${NETCDF_LIB_C} ${HDF5_LIB_2} ${HDF5_LIB_1} m z curl)
 set(INCLUDE_DIRS ${FFTW_INCLUDE_DIR} ${NETCDF_INCLUDE_DIR})
 
-add_definitions(-DRESTRICTKEYWORD=__restrict__)
-add_definitions(-DUSE_CBOOL)
+add_compile_definitions(RESTRICTKEYWORD=__restrict__)
+add_compile_definitions(RTE_RRTMGP_USE_CBOOL)
